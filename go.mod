80f7d9117ab88e24c85ab8f5c400dab378a4ff8b
tabo-syu <takeshima.nitkc@gmail.com>
2023-01-17 21:11:16 +0000

    🦺 locale のバリデータを実装

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/gin-gonic/gin v1.8.2
 	github.com/tabo-syu/riotgames-game-articles v0.1.2
+	golang.org/x/exp v0.0.0-20230116083435-1de6713980de
 )
 
 require (
@@ -23,7 +24,6 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/ugorji/go/codec v1.2.8 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
-	golang.org/x/exp v0.0.0-20230116083435-1de6713980de // indirect
 	golang.org/x/net v0.5.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
--- main.go
+++ main.go
@@ -5,12 +5,20 @@ import (
 
 	"github.com/gin-gonic/gin"
 	riotgames "github.com/tabo-syu/riotgames-game-articles"
+	"golang.org/x/exp/slices"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/league-of-legends/:locale", func(c *gin.Context) {
-		articles, err := riotgames.NewLOLWebsiteArticles(c.Param("locale"))
+		locale := c.Param("locale")
+		if !slices.Contains(riotgames.LOLLocales, locale) {
+			c.JSON(http.StatusNotFound, gin.H{
+				"message": "invalid locale",
+			})
+		}
+
+		articles, err := riotgames.NewLOLWebsiteArticles(locale)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"message": err.Error(),
@@ -21,7 +29,14 @@ func main() {
 	})
 
 	r.GET("/valorant/:locale", func(c *gin.Context) {
-		articles, err := riotgames.NewValorantWebsiteArticles(c.Param("locale"))
+		locale := c.Param("locale")
+		if !slices.Contains(riotgames.ValorantLocales, locale) {
+			c.JSON(http.StatusNotFound, gin.H{
+				"message": "invalid locale",
+			})
+		}
+
+		articles, err := riotgames.NewValorantWebsiteArticles(locale)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"message": err.Error(),
